11af4f35bc655775397d7dcb80d461bb8ee36394
Balázs <arpad.balazs93@hotmail.com>
2022-06-14 09:50:32 +0200

    generating signature

--- src/main/java/hu/ponte/hr/services/SignService.java
+++ src/main/java/hu/ponte/hr/services/SignService.java
@@ -13,19 +13,20 @@ import java.security.*;
 import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 @Service
 @Transactional
 public class SignService {
    public PrivateKey getPrivateKey() throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get("config/keys/key.private"));
+        byte[] keyBytes = Files.readAllBytes(Paths.get("src/main/resources/config/keys/key.private"));
         PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return kf.generatePrivate(spec);
     }
     public PublicKey getPublicKey() throws Exception {
-        byte[] keyBytes = Files.readAllBytes(Paths.get("config/keys/key.pub"));
-        PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
+        byte[] keyBytes = Files.readAllBytes(Paths.get("src/main/resources/config/keys/key.pub"));
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
         KeyFactory kf = KeyFactory.getInstance("RSA");
         return kf.generatePublic(spec);
     }
